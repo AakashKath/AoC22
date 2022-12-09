609072650a2281613c6d5922eae91896bfd0b704
kath <ge86guy@mytum.de>
2022-12-09 07:30:22 +0100

    day 9 finished

--- 9/sol.py
+++ 9/sol.py
@@ -1,4 +1,4 @@
-def update_position(direction, tracker, hp, tp):
+def update_direction(direction, hp):
     if direction == 'R':
         hp[1] += 1
     if direction == 'L':
@@ -7,6 +7,8 @@ def update_position(direction, tracker, hp, tp):
         hp[0] += 1
     if direction == 'D':
         hp[0] -= 1
+
+def update_position(tracker, hp, tp, to_be_tracked):
     if abs(hp[0]-tp[0]) <= 1 and abs(hp[1]-tp[1]) <= 1:
         return
     if hp[0] == tp[0]:
@@ -16,17 +18,21 @@ def update_position(direction, tracker, hp, tp):
     else:
         tp[0] += 1 if hp[0] > tp[0] else -1
         tp[1] += 1 if hp[1] > tp[1] else -1
-    tracker.add(tuple(tp))
+    if to_be_tracked:
+        tracker.add(tuple(tp))
 
 with open('input', 'r') as file:
     x = file.readlines()
-    tp = [0, 0]
-    hp = [0, 0]
+    position = [[0, 0] for _ in range(2)]
     tracker = set()
-    tracker.add(tuple(tp))
+    tracker.add((0, 0))
     for i in x:
         direction, steps = i.split('\n')[0].split(' ')
         for j in range(int(steps)):
-            update_position(direction, tracker, hp, tp)
+            for k in range(len(position)-1):
+                if k == 0:
+                    update_direction(direction, position[k])
+                update_position(tracker, position[k], position[k+1],
+                               True if k == len(position)-2 else False)
     print(len(tracker))
 
